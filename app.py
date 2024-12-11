396eaf283398d678e43064daabefde252a916ff7
Viviane Pereira <vivianesilper@gmail.com>
2024-12-11 14:14:26 -0300

    get.purchase_orders

--- app.py
+++ app.py
@@ -8,35 +8,27 @@ purchase_orders = [
         'description': 'Pedido de Compra 1',
         'items': [
             {
-                'id ': 1,
+                'id': 1,
                 'description': 'Item do pedido 1',
                 'price': 20.99
             }
         ]
     }
+
 ]
 
-# GET purchase_orders
-# GET purchase_orders_by_id
-# POST purchase_orders
-# GET purchase_orders_items
-# POST purchase_orders_items
+#GET purchase_orders
+#GET purchase_orders
+#POST purchase_orders
+#GET purchase_orders_items
+#POST purchase_orders_items
 
-@app.route('/')
+@app.route('/') #Decorator(Nenhum endpoint foi passado, então utilizaremos o localhost raiz)
 def home():
     return "Hello World! Alterado"
 
-@app.route('/purchase_orders')
+app.route('/purchase_orders')
 def get_purchase_orders():
-    return jsonify
-
-@app.route('/purchase_orders/<int:id>')
-def get_purchase_orders_by_id(id):
-    for po in purchase_orders:
-        if po['id'] == id:
-            return jsonify(po)
-    return jsonify({'message': 'Pedido {} não encontrado'.format(id)})
-
+    return jsonify(purchase_orders)
 
 app.run(port=5000)
-
