52e3b98505b03fbd2e68d83eade6b87ef2d18a42
Viviane Pereira <vivianesilper@gmail.com>
2024-12-11 14:41:30 -0300

    get purchase order by id

--- app.py
+++ app.py
@@ -27,8 +27,14 @@ purchase_orders = [
 def home():
     return "Hello World! Alterado"
 
-app.route('/purchase_orders')
+@app.route('/purchase_orders')
 def get_purchase_orders():
     return jsonify(purchase_orders)
 
+@app.route('/purchase_orders/<int:id>')
+def get_purchase_orders_by_id():
+    for po in get_purchase_orders:
+        if po ['id'] == id:
+         return jsonify(po)
+    return jsonify({'message': 'Pedido {} não encontrado'.format(id)})    
 app.run(port=5000)
